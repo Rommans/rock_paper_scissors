e2d426102c31dad16b27163f9d55fbdfcdf9920b
Rommans <rkokotko3@gmail.com>
2022-02-17 22:04:07 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,106 +1,135 @@
-$(document).ready(() => {
-    function evil(fn) {
-        return new Function('return ' + fn)();
+let chooseCon = document.querySelector('.choose')
+let resultCon = document.querySelector('.result');
+
+const start = () => {
+    let startCon = document.querySelector('.start');
+    startCon.addEventListener('click', (e) => {
+        e.preventDefault()
+        startCon.style.display = 'none'
+        chooseCon.style.display = 'block'
+    })
+}
+
+const handOptions = {
+    'rock': '✊',
+    'paper': '✋',
+    'scissors': '✌️'
+}
+
+const textOptions = {
+    'rock': 'Rock',
+    'paper': 'Paper',
+    'scissors': 'Scissors'
+}
+
+let SCORE = 0;
+let SCORE_COMP = 0;
+let ROUNDS = 0;
+let losserCount = 0;
+let winnerCount = 0;
+
+const userChoose = (hand) => {
+    chooseCon.style.display = 'none'
+    resultCon.style.display = 'block'
+    document.getElementById('userHand').innerHTML = handOptions[hand];
+    document.getElementById('userOption').innerHTML = textOptions[hand];
+    ROUNDS++;
+    document.getElementById('round').innerHTML = `Round ${ROUNDS}`
+    let compHand = compChoose();
+    winner(hand, compHand)
+
+    if(ROUNDS % 3 === 0) {
+        roundsWinner()
+    } else{
+        document.querySelector('.winner').style.display = 'none';
     }
+}
 
-    function calculator() {
-        let len = 0;
-        let arr = [];
-        let sum = 0;
-        let inputVal = document.getElementById('calculator-screen');
-        $('.calculator-keys .digit').on('click', function () {
-            let num = $(this).attr('value');
-            arr.push(num);
-            $('#calculator-screen').html(arr);
-            len = inputVal.innerHTML.split('');
-        });
+const compChoose = () => {
+    let result = ['rock', 'paper', 'scissors'];
+    let randItem = result[Math.floor(Math.random() * result.length)];
 
-        $('.calculator-keys .operator').on('click', function (e) {
-            e.preventDefault();
-            let ops = $(this).attr('value');
-            let lastEl = arr[arr.length - 1];
-            if (/[+-/*]/.test(ops) && /[+-/*]$/.test(lastEl)) {
-                arr.pop()
-                arr.push(ops);
-            } else {
-                arr.push(ops);
-            }
-            $('#calculator-screen').html(arr);
-            len = inputVal.innerHTML;
-            if (/(?=(\D{2}))/g.test(arr)) {
-                sum = len.substring(0, len.length - 1);
-                $('#calculator-screen').html(arr);
-            }
-        });
+    document.getElementById('compHand').innerHTML = handOptions[randItem];
+    document.getElementById('compOption').innerHTML = textOptions[randItem];
 
-        $('.equal-sign').on('click', () => {
-            let lastEl = arr[arr.length - 1];
-            let decimalNum = 3;
-            if (lastEl === '=') {
-                arr.pop()
-            }
-            try {
-                let total = evil(sum);
-                $('#calculator-screen').html(total % 1 !== 0 ? total.toFixed(decimalNum) : total);
-                if (sum === undefined) {
-                    $('#calculator-screen').html(0);
-                }
-                arr = [];
-                let logContainer = document.querySelector('#log');
-                let el = document.createElement('div');
-                el.setAttribute('class', 'sum');
-                if (len.charAt(0) === '=') {
-                    let a = len.substring(1)
-                    el.innerHTML = `
-                    <span class="circle"></span>
-                    <p class="item_log">${a}${total % 1 !== 0 ? total.toFixed(decimalNum) : total}</p>
-                    <span class="cross">╳</span>
-                `;
-                } else {
-                    el.innerHTML = `
-                    <span class="circle"></span>
-                    <p class="item_log">${len}${total % 1 !== 0 ? total.toFixed(decimalNum) : total}</p>
-                    <span class="cross">╳</span>
-                `;
-                }
-                logContainer.appendChild(el);
+    return randItem
+}
 
-                let undtext = document.querySelectorAll('.item_log')
-                undtext.forEach((el) => {
-                    const regExp = /48/
-                    if (regExp.test(el.innerText)) {
-                        el.classList.add('underlined')
-                    }
-                })
-            } catch (error) {
-                $('#calculator-screen').html(0);
-                sum = '';
-                arr = [];
-            }
-        });
-
-        $('.all-clear').click(() => {
-            sum = '';
-            arr = [];
-            $('#calculator-screen').html(0);
-            $('#calculator-screen').val(0);
-        });
-
-        $('#log').click((e) => {
-            if ($(e.target).hasClass('circle')) {
-                $(e.target).toggleClass('red');
-            }
-        });
-
-        $('#log').click((e) => {
-            if ($(e.target).hasClass('cross')) {
-                $(e.target).closest('.sum').remove();
-            }
-        });
-
-        $('#log').scroll(() => {
-            console.log(`Scroll Top: ${$('#log').scrollTop()}`)
-        });
+const winner = (userH, compH) => {
+    if (userH === 'paper' && compH === 'scissors') {
+        result('YOU LOSE!');
+        losserCount++;
+        SCORE_COMP++;
     }
-    calculator();
-});
+    if (userH === 'paper' && compH === 'rock') {
+        result('YOU WIN!');
+        winnerCount++;
+        SCORE++;
+    }
+    if (userH === 'paper' && compH === 'paper') {
+        result("It's a tie!");
+    }
+    if (userH === 'rock' && compH === 'scissors') {
+        result('YOU WIN!');
+        winnerCount++;
+        SCORE++;
+    }
+    if (userH === 'rock' && compH === 'paper') {
+        result('YOU LOSE!');
+        losserCount++;
+        SCORE_COMP++;
+    }
+    if (userH === 'rock' && compH === 'rock') {
+        result("It's a tie!");
+    }
+    if (userH === 'scissors' && compH === 'scissors') {
+        result("It's a tie!");
+    }
+    if (userH === 'scissors' && compH === 'rock') {
+        result('YOU LOSE!');
+        losserCount++;
+        SCORE_COMP++;
+    }
+    if (userH === 'scissors' && compH === 'paper') {
+        result('YOU WIN!');
+        winnerCount++;
+        SCORE++;
+    }
+    score(SCORE, SCORE_COMP)
+}
+
+const result = (res) => {
+    document.getElementById('winner').innerHTML = res;
+}
+
+const score = (scoreUser, scoreComp) => {
+    document.getElementById('score').innerHTML = scoreUser;
+    document.getElementById('scoreC').innerHTML = scoreComp;
+}
+
+const playAgain = () => {
+    chooseCon.style.display = 'block'
+    resultCon.style.display = 'none'
+}
+
+const reset = () => {
+    SCORE = 0;
+    SCORE_COMP = 0;
+    ROUNDS = 0;
+    document.getElementById('score').innerHTML = SCORE;
+    document.getElementById('scoreC').innerHTML = SCORE_COMP;
+    document.getElementById('round').innerHTML = ROUNDS;
+}
+
+const roundsWinner = () => {
+    document.querySelector('.winner').style.display = 'block';
+    if(winnerCount > losserCount) {
+        document.getElementById('winner__text').innerHTML = 'YOU WON!!!';
+    } else if (winnerCount < losserCount){
+        document.getElementById('winner__text').innerHTML = 'YOU LOST!!!';
+    } else {
+        document.getElementById('winner__text').innerHTML = 'TIE!!!';
+    }
+    winnerCount = 0;
+    losserCount = 0;
+}
